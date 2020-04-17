a8c81afee83e0bed478444a9af361aff9e3ef217
Hannes Pries <annonyme@annonyme.de>
2020-04-17 18:54:17 +0200

    WIP: add csv support

--- deploy/classes/mailcli/SendCommand.php
+++ deploy/classes/mailcli/SendCommand.php
@@ -4,44 +4,91 @@ namespace mailcli;
 
 use core\events\EventListenerFactory;
 use core\mail\SMTPMailerFactory;
+use core\twig\TwigFunctions;
 use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
 
-class SendCommand extends Command{
-    protected function configure(){
+class SendCommand extends Command
+{
+    protected function configure()
+    {
         $this->setName('mail:multi:send');
-        $this->setDescription('send multiple mails, with template support');
+        $this->setDescription('send multiple mails, with twig-template support');
 
         $this->addOption('from', null, InputOption::VALUE_REQUIRED, 'from email-adress');
         $this->addOption('fromname', null, InputOption::VALUE_OPTIONAL, 'from email-adress name');
-        $this->addOption('data', null, InputOption::VALUE_REQUIRED, 'email-adresses and data as (WIP: CSV or) JSON-objects in array. "mail" has to contain the email-adress you want to send the message to (use a object like "mail: "{"a@example.de": "Firstname Lastname"} to add the fullname).');
+        $this->addOption('data', null, InputOption::VALUE_REQUIRED, 'email-adresses and data as CSV or JSON-objects in array. "mail" has to contain the email-adress you want to send the message to (use a object like "mail: "{"a@example.de": "Firstname Lastname"} to add the fullname).');
         $this->addOption('subject', null, InputOption::VALUE_REQUIRED, 'subject as string, twig: or column:');
         $this->addOption('body', null, InputOption::VALUE_REQUIRED, 'subject as string, file: or column:');
         $this->addOption('ishtml', null, InputOption::VALUE_OPTIONAL, 'yes/no. default is no');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output){
-        $data = json_decode(file_get_contents($input->getOption('data')), true);
-        if(count($data) < 1) {
+    private function readCSV(string $filepath): array
+    {
+        $data = [];
+        $headers = [];
+
+        $firstRow = true;
+        if (($handle = fopen($filepath, "r")) !== FALSE) {
+            while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                if ($firstRow) {
+                    $headers = $row;
+                    $firstRow = false;
+                } else {
+                    $newRow = [];
+                    foreach ($row as $id => $value) {
+                        if (isset($headers[$id])) {
+                            $newRow[$headers[$id]] = $value;
+                        } else {
+                            $newRow[$id] = $value;
+                        }
+                    }
+                    $data[] = $newRow;
+                }
+            }
+            fclose($handle);
+        }
+        return $data;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|void|null
+     * @throws Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dataFilepath = $input->getOption('data');
+        $data = [];
+        if (preg_match("/\.csv$/i", $dataFilepath)) {
+            $data = $this->readCSV($dataFilepath);
+        } else {
+            $data = json_decode(file_get_contents($dataFilepath), true);
+        }
+
+        if (count($data) < 1) {
             throw new Exception('no data or invalid data-format! (' . $input->getOption('data') . ')');
         }
         $from = $input->getOption('from');
-        if(!filter_var($from, FILTER_VALIDATE_EMAIL)) {
+        if (!filter_var($from, FILTER_VALIDATE_EMAIL)) {
             throw new Exception('invalid from email-adress');
         }
 
         $fromName = $input->hasOption('fromname') ? $input->getOption('fromname') : null;
 
         $subject = $input->getOption('subject');
-        if(!$subject || strlen($subject) === 0){
+        if (!$subject || strlen($subject) === 0) {
             throw new Exception('missing subject');
         }
 
         $body = $input->getOption('body');
-        if(!$body || strlen($body) === 0){
+        if (!$body || strlen($body) === 0) {
             throw new Exception('missing body');
         }
 
@@ -56,32 +103,30 @@ class SendCommand extends Command{
             'isHtml' => $isHtml,
         ]);
 
-        foreach($options['data'] as $item) {
-            try{
-                if(!isset($item['mail'])) {
+        foreach ($options['data'] as $item) {
+            try {
+                if (!isset($item['mail'])) {
                     throw new Exception('no mail-adress found in data');
                 }
-                
+
                 $bodyTemplate = $options['body'];
-                if(preg_match("/^file:/i", $bodyTemplate)) {
+                if (preg_match("/^file:/i", $bodyTemplate)) {
                     $bodyTemplate = file_get_contents(preg_replace("/^file:/", '', $bodyTemplate));
-                }
-                else if(preg_match("/^column:/i", $bodyTemplate)) {
+                } else if (preg_match("/^column:/i", $bodyTemplate)) {
                     $bodyTemplate = $item[preg_replace("/^file:/", '', $bodyTemplate)];
                 }
 
                 $subjectTemplate = $options['subject'];
-                if(preg_match("/^file:/i", $subjectTemplate)) {
+                if (preg_match("/^file:/i", $subjectTemplate)) {
                     $subjectTemplate = file_get_contents(preg_replace("/^file:/", '', $subjectTemplate));
-                }
-                else if(preg_match("/^column:/i", $subjectTemplate)) {
+                } else if (preg_match("/^column:/i", $subjectTemplate)) {
                     $subjectTemplate = $item[preg_replace("/^file:/", '', $subjectTemplate)];
                 }
 
-                $loader = new \Twig_Loader_Array( [ 'body' => $bodyTemplate, 'subject' => $subjectTemplate]);
-                $twig = new \Twig_Environment($loader);
-                $twig = \core\twig\TwigFunctions::decorateTwig($twig);
-                
+                $loader = new ArrayLoader(['body' => $bodyTemplate, 'subject' => $subjectTemplate]);
+                $twig = new Environment($loader);
+                $twig = TwigFunctions::decorateTwig($twig);
+
                 $outputSubject = $twig->render('subject', $item);
                 $outputSubject = EventListenerFactory::getInstance()->fireFilterEvent('multimail_rendered_subject', $outputSubject, ['mail' => $item, 'renderer' => $twig, 'template' => $subjectTemplate]);
                 $outputBody = $twig->render('body', $item);
@@ -99,10 +144,9 @@ class SendCommand extends Command{
                 ]);
 
                 $output->writeln($item['mail'] . ': ok');
-            }
-            catch(Exception $e) {
+            } catch (Exception $e) {
                 $output->writeln($item['mail'] . ': failed (' . $e->getMessage() . ')');
-            }    
+            }
         }
     }
 }
